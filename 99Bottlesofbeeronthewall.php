fba63c0259a52a179a2a3d1ae6b6da3b50319c22
mamtabajaj <mamta.bajaj8@gmail.com>
2024-10-01 21:29:23 -0400

    Change count from 90 to 78

--- 99Bottlesofbeeronthewall.php
+++ 99Bottlesofbeeronthewall.php
@@ -5,8 +5,8 @@
 </head>
 <body>
 <?php
-//we start with 99 bottles and keep on decrementing count until we get 1 bottles
-for ($bottles = 90; $bottles >= 1; $bottles--) {
+//we start with 78 bottles and keep on decrementing count by 78 until we get 1 bottles
+for ($bottles = 78; $bottles >= 1; $bottles--) {
     if ($bottles > 1) { // we have more than 1 bottles of beer
         echo "$bottles bottles of beer on the wall, $bottles bottles of beer.<br>";
         echo "Take one down, pass it around, " . (($bottles - 1)) . (($bottles - 1) == 1 ? " bottle":" bottles") . " of beer on the wall.<br><br>";
